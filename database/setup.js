b984998fcb7ab298b0878304170e448ed80248ec
Aefrly <fraley.allison@ic.edu>
2026-03-17 08:26:21 -0500

    Created new tables for Users and Checkout

--- database/setup.js
+++ database/setup.js
@@ -39,8 +39,67 @@ const Book = db.define('Book', {
     }
 });
 
+// Define User model
+const User = db.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  membershipDate: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  }
+});
+
+// Define Checkout model
+const Checkout = db.define('Checkout', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  bookId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  checkoutDate: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  dueDate: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  returnDate: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  isReturned: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+});
+
 // Export for use in other files
-module.exports = { db, Book };
+module.exports = { db, Book, User, Checkout };
 
 // Create database and tables
 async function setupDatabase() {
